a5e45d6a82ff77d3c5bbb966a0e0274dd63d1a01
Lauty128 <lautarosilverii@gmail.com>
2023-11-10 16:20:43 -0300

    presentation text for the home page added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom";
+
+
 export default function Home(){
 
     return (
-        <h2 style={{ marginLeft:'2em' }}>
-            Bienvenido a la interfaz de <b>ProveeMed</b>!!!
-        </h2>
+        <div style={{ marginLeft:'2em' }}>
+            <h2>
+                Bienvenido a la interfaz de <b>ProveeMed</b>!!
+            </h2>
+
+            <p style={{color:'#505050', fontSize:'1.2em', maxWidth:'1100px'}}>
+            Simplificamos el acceso a información crucial sobre proveedores argentinos y 
+            sus equipos médicos. Descubre una amplia gama de dispositivos, encuentra 
+            proveedores confiables y mantén tus datos actualizados fácilmente. 
+            Nuestra sección de backups te ofrece control total, permitiéndote descargar 
+            plantillas, cargar actualizaciones y revisar el historial de cambios. Con nosotros,
+            la gestión eficiente de equipos médicos nunca ha sido tan fácil y centralizada. 
+            ¡Explora y optimiza tu experiencia ahora!
+            </p>
+
+            <p style={{ marginTop:'2em', color:'#505050', fontSize:'1.2em', maxWidth:'1100px' }}>
+                Haz {' '}
+                <Link to={'/backups'}>click aquí</Link>
+                {' '}para respaldar tus datos y asegurar una gestión sin contratiempos!
+            </p>
+        </div>
     )
 }
